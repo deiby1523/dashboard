f6efbfd2cd13029abf1c28bdffe00d65c03fe6ab
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-05-11 15:42:04 -0500

    Working on Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom';
+import { siShopware } from 'react-icons/si';
+import { MdOutLineCancel } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { links } from './../data/dummy';
 
 const Sidebar = () => {
+  const activeMenu = true;
   return (
-    <div>Sidebar</div>
+    <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
+      {activeMenu && (<>
+        <div className='flex justify-between items-center'>
+          <Link to='./' onClick={ () => {} }></Link>
+        </div>
+      </>)}
+    </div>
   )
 }
 
