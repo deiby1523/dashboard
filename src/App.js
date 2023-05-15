df538c775e1dbd267f9a02aa5ac48532f2949d0c
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-05-14 22:13:16 -0500

    Working on useStateContext

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import { Navbar, Footer,Sidebar,ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, Line, ColorPicker, ColorMapping, Editor} from './pages'
 import './App.css';
 
+import { useStateContext} from './contexts/ContextProvider';
+
 const App = () => {
-    const activeMenu = true;
+    const { activeMenu } = useStateContext();
 
     return (
         <div>
