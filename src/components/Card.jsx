f43c31234e9c30d7f93580b4ac175b1e37b38b23
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-05-16 23:35:24 -0500

    Card component creation

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = ({ title, image, description }) => {
+  return (
+    <div className="card">
+      <img src={image} alt={title} className="card-image" />
+      <div className="card-content">
+        <h2 className="card-title">{title}</h2>
+        <p className="card-description">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
