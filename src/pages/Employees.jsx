99b182c6577cf1c1ff75070a5417724212cb0056
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-05-26 22:02:06 -0500

    Employees page implementation

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+// Syncfusion
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Inject } from '@syncfusion/ej2-react-grids';
+// Data
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
+import { Toolbar } from '@syncfusion/ej2-react-richtexteditor';
 
 const Employees = () => {
+  const toolbarOptions = ['Search'];
+  const editing = { allowDeleting: true, allowEditing: true };
+
   return (
-    <div>Employees</div>
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees"/>
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={toolbarOptions}
+        pageSettings={{ pageCount: 5 }}
+        editSettings={editing}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index}{...item}/>
+          ))}
+        </ColumnsDirective>
+        <Inject services={ [Page, Search]}/>
+      </GridComponent>
+    </div>
   )
 }
 
