048bf17454bb23ee90be161e01c3e7542ec197cd
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-06-06 07:43:11 -0500

    Fix sidebar problem

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ import { links } from './../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize, currentColor} = useStateContext();
 
 	const handleCloseSideBar = () => {
     if (activeMenu && screenSize <= 900) {
@@ -37,7 +37,7 @@ const Sidebar = () => {
                 <p className='text-gray-400 m-3 mt-4 uppercase'>
                     {item.title}
                     {item.links.map((link) => (
-                    <NavLink to={`/${link.name}`} key={link.name} onClick={ handleCloseSideBar } className={({ isActive }) => isActive ? activeLink : normalLink }>
+                    <NavLink to={`/${link.name}`} key={link.name} onClick={ handleCloseSideBar } style={({isActive}) => ({backgroundColor : isActive ? currentColor : ''})} className={({ isActive }) => isActive ? activeLink : normalLink }>
                      {link.icon}
                      <span className='capitalize'>
                         {link.name}
