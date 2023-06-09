8ce00aecf775f395fce18db0e06d2baa2fa09b1a
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-06-09 17:43:57 -0500

    LineChart almost finished

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { ChartHeader, LineChart } from "../../components";
+import { Header } from '../../components';
 
 const Line = () => {
   return (
-    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white dark:bg-secondary-bg rounded-3xl'>
-      <Header category="Charts" title="Line"/>
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Chart" title="Inflation"/>
+      <div className="w-full">
+        <LineChart/>
+      </div>
     </div>
   )
 }
