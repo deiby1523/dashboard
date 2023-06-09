78214ae3e456b5fdd90003393e46e55b8cdc5f1d
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-06-08 23:38:56 -0500

    Start of the creation of the chart(line) component

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { ChartHeader, LineChart } from "../../components";
 
 const Line = () => {
   return (
-    <div>Line</div>
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white dark:bg-secondary-bg rounded-3xl'>
+      <Header category="Charts" title="Line"/>
+    </div>
   )
 }
 
