b872a453350ab2f25c74222542c43c021a07cd9f
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-08-26 21:41:34 -0500

    Update of navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const NavButton = ({ title,customFunc, icon, color, dotColor}) => (
   
 )
 
-// Navbar Component
+// Navbar
 
 const Navbar = () => {
   const { activeMenu, setActiveMenu, handleClick, isClicked, setIsClicked, screenSize, setScreenSize, currentColor } = useStateContext();
